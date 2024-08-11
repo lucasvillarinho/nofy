bf56ad469bc4ebd34c49e46869181aa4be457c47
Villa <lucas.sbv5@yahoo.com.br>
2024-08-11 19:27:33 -0300

    feat: Update Slack token and channel ID in slack.go

--- examples/slack/slack.go
+++ examples/slack/slack.go
@@ -11,13 +11,13 @@ func main() {
 	// Create a new Slack messenger
 	slackMessenger, _ := slack.NewSlackMessenger(
 		// Set the Slack token to be used to send (required)
-		slack.WithToken("test-token"),
+		slack.WithToken("token"),
 		slack.WithMessage(
 			// Message to be sent to the slack channel (required)
 			// The message is a slice of maps, each map represents a block of the message
 			// In this case, we are sending a single block with a text section
 			slack.Message{
-				Channel: "test-channel",
+				Channel: "channelID",
 				Content: []map[string]interface{}{
 					{
 						"type": "section",
