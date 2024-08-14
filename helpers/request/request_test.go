6a6a854f73e436318d26acc74bc6a06b92fb63ad
Villa <lucas.sbv5@yahoo.com.br>
2024-08-14 19:20:27 -0300

    test: Methods -> WithMethod, WithClient,  WithURL, WithHeader, WithTimeout and WithPayload

--- helpers/request/request_test.go
+++ helpers/request/request_test.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"net/http"
 	"testing"
 	"time"
 
@@ -19,7 +20,6 @@ func TestValidate(t *testing.T) {
 		assert.IsNil(t, err)
 	})
 	t.Run("Invalid request - missing method", func(t *testing.T) {
-
 		r := &Request{
 			URL:     "https://example.com",
 			Timeout: 500 * time.Second,
@@ -48,5 +48,59 @@ func TestValidate(t *testing.T) {
 		err := validate(r)
 		assert.IsNotNil(t, err)
 		assert.AreEqual(t, err.Error(), "timeout is required")
+	})
+}
+
+func TestWith(t *testing.T) {
+	t.Run("With client", func(t *testing.T) {
+		r := &Request{}
+		client := &http.Client{}
+
+		WithClient(client)(r)
+
+		assert.AreEqual(t, r.Client, client, "Expected client to be set")
+	})
+
+	t.Run("With method", func(t *testing.T) {
+		r := &Request{}
+
+		WithMethod("GET")(r)
+
+		assert.AreEqual(t, r.Method, "GET", "Expected method to be set")
+	})
+
+	t.Run("With URL", func(t *testing.T) {
+		r := &Request{}
+
+		WithURL("https://example.com")(r)
+
+		assert.AreEqual(t, r.URL, "https://example.com", "Expected URL to be set")
+	})
+
+	t.Run("With header", func(t *testing.T) {
+		r := &Request{}
+
+		WithHeader("key", "value")(r)
+
+		assert.AreEqual(t, r.Headers["key"], "value", "Expected header to be set")
+	})
+
+	t.Run("With timeout", func(t *testing.T) {
+		r := &Request{}
+
+		WithTimeout(500 * time.Second)(r)
+
+		assert.AreEqual(t, r.Timeout, 500*time.Second, "Expected timeout to be set")
+	})
+
+	t.Run("With payload", func(t *testing.T) {
+		r := &Request{}
+		payload := map[string]interface{}{
+			"key": "value",
+		}
+
+		WithPayload(payload)(r)
+
+		assert.AreEqual(t, r.Payload["key"], "value", "Expected payload to be set")
 	})
 }
